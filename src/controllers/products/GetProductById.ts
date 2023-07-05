1f6887d6761e05e750a39e4bcea05b433217b393
Cr-Israel <carlosisrael08@hotmail.com>
2023-07-05 13:19:00 -0300

    feature: added validations

--- src/controllers/products/GetProductById.ts
+++ src/controllers/products/GetProductById.ts
@@ -1,12 +1,24 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 import Product from '../../models/Product';
-import { ObjectId } from 'mongoose';
+import mongoose from 'mongoose';
 
 export async function GetProductById(req: FastifyRequest, reply: FastifyReply) {
     const { id } = req.params as {
-        id: ObjectId
+        id: mongoose.ObjectId
     };
 
-    const product = await Product.findById(id);
-    reply.code(200).send(product);
+    // Check if ID is valid
+    if (!mongoose.isValidObjectId(id)) {
+        reply.code(422).send({ message: 'Produto com identificador inválido!' });
+        return;
+    };
+
+    // Check if Product Exist
+    const product = await Product.findOne({ _id: id });
+    if (!product) {
+        reply.code(422).send({ message: 'Produto não existente!' });
+        return;
+    };
+
+    reply.code(200).send({ product });
 };
