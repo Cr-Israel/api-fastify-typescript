20991fab437f37a68016459ca979c7edd53a1794
Cr-Israel <carlosisrael08@hotmail.com>
2023-07-05 11:03:52 -0300

    feature: routes file

--- src/routes/products/productRoutes.ts
+++ src/routes/products/productRoutes.ts
@@ -1,7 +1,13 @@
 import { FastifyInstance } from 'fastify'
 import { Create } from '../../controllers/products/Create';
+import { GetProducts } from '../../controllers/products/GetProducts';
+import { GetProductById } from '../../controllers/products/GetProductById';
 
 export async function productRoutes(server: FastifyInstance) {
+    // Create a Product
     server.post('/', Create);
-    // server.get('/', Create);
+    // Get All Products
+    server.get('/', GetProducts);
+    // Get a Single Product
+    server.get('/:id', GetProductById);
 };
