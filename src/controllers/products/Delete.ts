eb8d988e9ee7b83d706f4b307c16c4f0b3028e86
Cr-Israel <carlosisrael08@hotmail.com>
2023-07-05 13:20:55 -0300

    feature: added validations

--- src/controllers/products/Delete.ts
+++ src/controllers/products/Delete.ts
@@ -1,10 +1,23 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 import Product from '../../models/Product';
-import { ObjectId } from 'mongoose';
+import mongoose from 'mongoose';
 
 export async function Delete(req: FastifyRequest, reply: FastifyReply) {
     const { id } = req.params as {
-        id: ObjectId
+        id: mongoose.ObjectId
+    };
+
+    // Check if ID is valid
+    if (!mongoose.isValidObjectId(id)) {
+        reply.code(422).send({ message: 'Produto com identificador inválido!' });
+        return;
+    };
+
+    // Check if Product Exist
+    const product = await Product.findOne({ _id: id });
+    if (!product) {
+        reply.code(422).send({ message: 'Produto não existente!' });
+        return;
     };
 
     try {
