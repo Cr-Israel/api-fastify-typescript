7c3be0db9fb28b26081b5ff7d68e5f0d93e9858e
Cr-Israel <carlosisrael08@hotmail.com>
2023-07-05 10:35:21 -0300

    feature: fix the error to connect to cluster mongo

--- src/database/products/product.model.ts
+++
@@ -1,5 +0,0 @@
-import { model } from 'mongoose';
-import { IProductDocument } from './product.types';
-import ProductSchema from './product.schema';
-
-export const ProductModel = model<IProductDocument>('product', ProductSchema);
--- src/database/products/product.schema.ts
+++
@@ -1,9 +0,0 @@
-import { Schema } from 'mongoose';
-
-const ProductSchema = new Schema({
-    name: String,
-    price: Number,
-    description: String
-});
-
-export default ProductSchema;
